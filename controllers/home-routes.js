174c036725f4302c3904390b0c4754fe593b61b6
Matt Linder <mjlinder218@gmail.com>
2021-07-01 20:00:27 -0700

    Fixed res.render statement

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,14 +3,41 @@ const Article = require('../models/Article');
 
 // route to get all articles
 router.get('/', async (req, res) => {
-    const articleData = await Article.findAll().catch((err) => { 
-        res.json(err);
-      });
-        const articles = articleData.map((article) => article.get({ plain: true }));
-        res.render('all', { articles });
-      });
-  
-  // route to get one article
+  const articleData = await Article.findAll().catch((err) => { 
+    res.json(err);
+  });
+    const articles = articleData.map((article) => article.get({ plain: true }));
+    res.render('all', { articles });
+});
+
+// GET one article
+// router.get('/article/:id', withAuth, async (req, res) => {
+//   try {
+//     const dbArticleData = await Article.findByPk(req.params.id, {
+//       include: [
+//         {
+//           model: Article,
+//           attributes: [
+//             'id',
+//             'article_title',
+//             'article_content',
+//             'article_author',
+//             'article_createDate',
+//           ],
+//         },
+//       ],
+//     });
+
+//     const article = dbArticleData.get({ plain: true });
+//     res.render('article', { article, loggedIn: req.session.loggedIn });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
+// *******
+// route to get one article
   router.get('/article/:id', async (req, res) => {
     try{ 
         const articleData = await Article.findByPk(req.params.id);
@@ -19,7 +46,7 @@ router.get('/', async (req, res) => {
             return;
         }
         const article = articleData.get({ plain: true });
-        res.render('article', article);
+        res.render('article', { article });
       } catch (err) {
           res.status(500).json(err);
       };     
