7403d8834e056e444ce8646950c955d55a1267b4
Matt Linder <mjlinder218@gmail.com>
2021-07-04 09:36:01 -0700

    Made fix to get article content to show up in edit fields

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -70,14 +70,42 @@ router.get('/', async (req, res) => {
     res.render('login');
   });
 
-  router.get('/edit/:id', (req, res) => {
-    if (req.session.loggedIn) {
-      res.redirect('/');
-      return;
-    }
+  // router.get('/edit/:id', (req, res) => {
+  //   if (req.session.loggedIn) {
+  //     res.redirect('/');
+  //     return;
+  //   }
   
-    res.render('editarticle');
+  //   res.render('editarticle');
+  // });
+  router.get('/edit/:id', async (req, res) => {
+    try{ 
+        const articleData = await Article.findByPk(req.params.id);
+        if(!articleData) {
+            res.status(404).json({message: 'No article with this id!'});
+            return;
+        }
+        const article = articleData.get({ plain: true });
+        res.render('editArticle', article);
+      } catch (err) {
+          res.status(500).json(err);
+      };     
   });
+  // route to get one dish
+  // router.get('/dish/:id', async (req, res) => {
+  //   try{ 
+  //       const dishData = await Dish.findByPk(req.params.id);
+  //       if(!dishData) {
+  //           res.status(404).json({message: 'No dish with this id!'});
+  //           return;
+  //       }
+  //       const dish = dishData.get({ plain: true });
+  //       res.render('dish', dish);
+  //     } catch (err) {
+  //         res.status(500).json(err);
+  //     };     
+  // });
+
 
   router.get('/signup', (req, res) => {
     if (req.session.loggedIn) {
