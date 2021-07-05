5f54032bc5d14c2d2dd35b9bae2680b8be9d999b
Matt Linder <mjlinder218@gmail.com>
2021-07-05 10:08:51 -0700

    Added userRoutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-
+const userRoutes = require('./userRoutes');  
 const articleRoutes = require('./article-routes');
 
+router.use('/users', userRoutes);  
 router.use('/article', articleRoutes);
 
 module.exports = router;
