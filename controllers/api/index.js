68f26c7f63fc32e03fdc28364631c025ee82d383
Matt Linder <mjlinder218@gmail.com>
2021-07-06 16:52:44 -0700

    Added comment routes code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');  
 const articleRoutes = require('./article-routes');
+const commentRoutes = require('./comment-routes');
 
 router.use('/users', userRoutes);  
 router.use('/article', articleRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
